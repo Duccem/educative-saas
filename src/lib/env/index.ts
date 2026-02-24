9988c7284778df059edff93b72c08a74e461a694
Jose Veliz <ducen29@gmail.com>
2026-02-23 23:53:05 -0400

    feat(database): add initial database schema and migrations
    - Introduced new database schema with tables for user, session, account, organization, member, invitation, and verification.
    - Added migration scripts to create the necessary tables and relationships.
    - Integrated drizzle-orm with Neon database for ORM functionality.
    - Updated environment configuration to include DATABASE_URL.
    - Added new dependencies for dotenv, drizzle-orm, and Neon database support.

--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -1,9 +1,13 @@
 import { createEnv } from "@t3-oss/env-core";
+import z from "zod";
 
 export const env = createEnv({
-  server: {},
+  server: {
+    DATABASE_URL: z.string(),
+  },
   client: {},
   clientPrefix: "",
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
 });
+
