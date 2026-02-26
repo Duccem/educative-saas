f7a342cd3d0ffa7de582cb405663f0ced7e101e1
Jose Veliz <ducen29@gmail.com>
2026-02-25 23:33:18 -0400

    feat: implement sign-up form with validation, social login options, and user feedback

--- src/app/(app)/(auth)/sign-up/page.tsx
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -1,4 +1,16 @@
+import { SignUpForm } from "@/components/sections/auth/sign-up-form";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-6 w-2/3">
+      <div className="space-y-2 text-center">
+        <h2 className="text-3xl text-foreground">Create Account</h2>
+        <p className="text-muted-foreground">
+          Create a new account to get started with AcadAI.
+        </p>
+      </div>
+      <SignUpForm />
+    </div>
+  );
 }
 
