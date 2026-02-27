b49b5e5c0a268dd10a13b6092948d9dc3cd79cdb
Jose Veliz <ducen29@gmail.com>
2026-02-27 17:31:34 -0400

    feat: add InviteMember and ListPendingInvitations components; update TeamSettingsPage to include them

--- src/app/(app)/(dashboard)/settings/team/page.tsx
+++ src/app/(app)/(dashboard)/settings/team/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <div className="p-4"></div>;
+import InviteMember from "@/components/sections/organization/invite-member";
+import { ListOrganizationMembers } from "@/components/sections/organization/list-organization-members";
+import ListPendingInvitations from "@/components/sections/organization/list-pending-invitations";
+
+export default function TeamSettingsPage() {
+  return (
+    <div className="flex w-full  mx-auto flex-col gap-6 pb-4">
+      <div>
+        <h1 className="text-2xl font-semibold leading-none">Team Settings</h1>
+        <p className=" text-muted-foreground">Manage your team settings.</p>
+      </div>
+      <InviteMember />
+      <ListPendingInvitations />
+      <ListOrganizationMembers />
+    </div>
+  );
 }
+
